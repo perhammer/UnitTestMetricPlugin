fad48c5c1e51df60464c95a5568ea87e9ea9b5a9
katlford31 <katlford31@hotmail.co.uk>
2018-08-13 08:24:22 +0100

    pom + PlugIn alteration
    Taking out parts of pom that wasnt working
    Added in attempt to call toolmain class in plugin main, not working currently

--- src/main/java/UnitTestMetricPlugIn/PlugInMain.java
+++ src/main/java/UnitTestMetricPlugIn/PlugInMain.java
@@ -1,20 +1,22 @@
 package UnitTestMetricPlugIn;
 
-import org.apache.maven.plugin.*;
-
-
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.descriptor.Parameter;
 import org.apache.maven.plugins.annotations.Mojo;
-
+	
 @Mojo(name = "metrictesting")
 
-public class PlugInMain extends AbstractMojo 	{
+public class PluginMain extends AbstractMojo 	{
 	
-    public void execute() throws MojoExecutionException
-    {
-    	/*Hopefully should run an instance of the bytecodereader class when plugin is built 
-    	Not been able to test due to issues with running the plugin via command line and plug in set up
-    	 */
-    	getLog().info( "Code inspection for unit test classes" );
-       
-    }
-}
+	Parameter 
+	ToolMain;
+	
+	    public void execute() throws MojoExecutionException
+	    {
+	    	getLog().info( "Code inspection for unit test classes" );
+	    	ToolMain.getClass();
+	    }
+	}
+
+
