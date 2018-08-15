fcda6fa3b6704857bbaf0712c818d1c9c087734d
katlford31 <katlford31@hotmail.co.uk>
2018-08-15 22:07:47 +0100

    comment on plugin main issue
    PlugIn running different. supposed to be deleted, version of pluginMain

--- src/main/java/UnitTestMetricPlugIn/PlugInMain.java
+++ src/main/java/UnitTestMetricPlugIn/PlugInMain.java
@@ -14,6 +14,10 @@ public class PlugInMain extends AbstractMojo 	{
 	
 	    public void execute() throws MojoExecutionException
 	    {
+	    	/*Plugin running a different version of this class, a deleted one, no changes made 
+	    	 * to this class are being reflected in the running of the plugin for instance the 
+	    	 * log output still reads code inspection for unit test classes 
+	    	 */
 	    	getLog().info( "A Code inspection tool for unit test classes" );
 	    	ToolMain.getClass();
 	    }
