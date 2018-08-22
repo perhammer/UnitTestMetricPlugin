b6e5095e4e12bc03cdb680b03488ded5d79034c2
katlford31 <katlford31@hotmail.co.uk>
2018-08-22 23:28:55 +0100

    update to readme and plugin main
    Plugin still a work in progress

--- src/main/java/UnitTestMetricPlugIn/PlugInMain.java
+++ src/main/java/UnitTestMetricPlugIn/PlugInMain.java
@@ -9,16 +9,11 @@ import org.apache.maven.plugins.annotations.Mojo;
 
 public class PlugInMain extends AbstractMojo 	{
 	
-	Parameter 
-	ToolMain;
 	
 	    public void execute() throws MojoExecutionException
 	    {
-	    	/*Plugin running a different version of this class, a deleted one, no changes made 
-	    	 * to this class are being reflected in the running of the plugin for instance the 
-	    	 * log output still reads code inspection for unit test classes 
-	    	 */
+	    	
 	    	getLog().info( "A Code inspection tool for unit test classes" );
-	    	ToolMain.getClass();
+	    	ToolMain runthemain = new ToolMain(); 
 	    }
 	}
